e9735dd68456d7e8cc6fc9ab74ecfbb0b800bbec
Daniel Vega <danve4@gmail.com>
2016-11-23 02:06:50 -0600

    Added support for text with multiple words

--- site.js
+++ site.js
@@ -3,7 +3,7 @@ $.noConflict();
   $(document).ready(function() {
      $('#uc-form').on('submit', function(e) {
       var tkn = '68b2621e05c8479086e984a98ea8e716';
-      var text = $('#uc-text').val();
+      var text = ($('#uc-text').val()).replace(/ /g, "%20");
       var query = 'https://api.dandelion.eu/datatxt/nex/v1/?text='+text+'&token='+tkn+'&lang=en';
 
       $.ajax({
